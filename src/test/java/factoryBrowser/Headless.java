3363abd9a3488350f1d3660a6584ab3b7f5f1a89
SpitaleA <agute.jcr@gmail.com>
2022-11-21 20:12:17 -0300

    Linux chrome test

--- src/test/java/factoryBrowser/Headless.java
+++ src/test/java/factoryBrowser/Headless.java
@@ -1,5 +1,6 @@
 package factoryBrowser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,16 +11,25 @@ public class Headless implements IBrowser {
     @Override
     public WebDriver create() {
         //Esto settea el path al archivo driver webdriver
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriverLinux.exe");
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
+
+
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
 
         //Para correr en headless
-        ChromeOptions options = new ChromeOptions();
+        /*ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         ChromeDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));*/
 
         return driver;
     }
